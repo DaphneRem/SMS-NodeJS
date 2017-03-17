30dc3f7c9a90e55949536e6194c52814671aaf50
daphne remali <remalidaphne@gmail.com>
2017-03-17 13:58:42 +0100

    add key from nexmo

---
+++ server/key.js
@@ -0,0 +1,5 @@
+'use strict';
+
+var keyApi = 'xxxxxxxx'; // remplacer xxxxxxxx par l'API key de votre compte nexmo (settings)
+var secretApi = '****************'; // remplacer **************** par l'API secret de votre compte nexmo (settings)
+var myNumber = 611111111 ; // remplacer 11111111 par votre numéro de téléphone sans le 0 du début
